d36520efb3b8502706309e47d2f3275115e942e5
moyv <1354760865@qq.com>
2020-07-03 23:06:15 +0800

    完结

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,98 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <a-input :value="inputValue" @change="handleInputChange" placeholder="请输入任务" class="my_ipt" />
+    <a-button @click="addItemToList" type="primary">添加事项</a-button>
+
+    <a-list bordered :dataSource="infoList" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox :checked="item.done" @change="(e)=>{cbStatusChanged(e,item.id)}">{{item.info}}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions" @click="removeItemById(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{unDoneLength}}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button @click="changeList('all')" :type="viewkey === 'all' ? 'primary' : 'default' ">全部</a-button>
+          <a-button @click="changeList('undone')" :type="viewkey === 'undone' ? 'primary' : 'default' ">未完成</a-button>
+          <a-button @click="changeList('done')" :type="viewkey === 'done' ? 'primary' : 'default' ">已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a @click="clean">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
-<style>
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  name: "app",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(["list", "inputValue", "viewkey"]),
+    ...mapGetters(["unDoneLength","infoList"])
+  },
+  methods: {
+    handleInputChange(e) {
+      //console.log(e.target.value)
+      this.$store.commit("setInputValue", e.target.value);
+    },
+    addItemToList() {
+      if (this.inputValue.trim().length <= 0) {
+        return this.$message.warning("文本框不能为空");
+      }
+      this.$store.commit("addItem");
+    },
+    removeItemById(id) {
+      this.$store.commit("removeItem", id);
+    },
+    cbStatusChanged(e, id) {
+      const param = {
+        id: id,
+        status: e.target.checked
+      };
+      this.$store.commit("changeStatus", param);
+    },
+    clean() {
+      this.$store.commit("cleanDone");
+    },
+    changeList(key) {
+      //console.log(key)
+      this.$store.commit("changeViewKey", key);
+    }
+  },
+  created() {
+    this.$store.dispatch("getList");
+  }
+};
+</script>
+
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  padding: 10px;
 }
 
-#nav {
-  padding: 30px;
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,76 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
-
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    list: [],
+    inputValue: 'aaa',
+    nextId: 5,
+    viewkey:'all'
+  },
+  mutations: {
+    initList(state, list) {
+      state.list = list
+    },
+    setInputValue(state, val) {
+      state.inputValue = val
+    },
+    addItem(state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputValue = ''
+    },
+    removeItem(state, id) {
+      const i = state.list.findIndex(x => x.id === id)
+      if (i !== -1) {
+        state.list.splice(i, 1)
+      }
+    },
+    changeStatus(state, param) {
+      const i = state.list.findIndex(x => x.id === param.id)
+      if (i !== -1) {
+        state.list[i].done = param.status
+      }
+    },
+    cleanDone(state) {
+      state.list = state.list.filter(x => x.done === false)
+    },
+    changeViewKey(state,key){
+      state.viewkey = key
+    }
+  },
+  actions: {
+    getList(context) {
+      axios.get('/list.json').then(({ data }) => {
+        //console.log(data);
+        context.commit('initList', data)
+      })
+    }
+  },
+  getters: {
+    unDoneLength(state) {
+      return state.list.filter(x => x.done === false).length
+    },
+    infoList(state){
+      if(state.viewkey === 'all'){
+        return state.list
+      }
+      if(state.viewkey === 'undone'){
+        return state.list.filter(x=>!x.done)
+      }
+      if(state.viewkey === 'done'){
+        return state.list.filter(x=>x.done)
+      }
+      return state.list
+    }
+
+  },
   modules: {}
 });
